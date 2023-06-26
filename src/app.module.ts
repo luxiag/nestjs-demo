2c707eaee0350df951bd2a34cf2b2f74252bb86b
卢祥 <example@qq.com>
2023-06-26 11:22:19 +0800

    配置 typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,38 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import Configuration from './configuration';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const envFilePath = `.env.${process.env.NODE_ENV || `development`} `;
+
+console.log(envFilePath, 'aa');
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      //envFilePath
+      // envFilePath,
+      // load: [() => dotenv.config({ path: envFilePath })],
       load: [Configuration],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_PORT: Joi.number().default(3306),
+        DB_HOST: Joi.string().ip(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'testdb',
+      entities: [],
+      synchronize: true,
+      logging: ['error'],
     }),
     UserModule,
   ],
